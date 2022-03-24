c40039dee4fb0926cc89f315ee16d70ab0eb1c26
philip-hru <philip.huz11@gmail.com>
2022-03-24 19:30:33 +0100

    добавил java script добавил диалог(модальное окно)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 var pigs_number = 0;
-
-
+var dialog = document.querySelector('dialog')
+ var dialog = document.querySelector('dialog')
+ 
 function add_pig() {
     pigs_number = pigs_number + 1
     var circle_on_card = document.querySelector('.circle_on_card')
     circle_on_card.innerHTML = pigs_number
 }
-
+function show_dialog() {
+    dialog.showModal();
+}
+function close_dialog() {
+    dialog.close();
+}
