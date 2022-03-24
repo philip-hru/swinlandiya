91c892fe87a98978ec6bf36dd7713ea6a12ddde7
philip-hru <philip.huz11@gmail.com>
2022-03-24 19:43:50 +0100

    поправил java script

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 var pigs_number = 0;
 var dialog = document.querySelector('dialog')
- var dialog = document.querySelector('dialog')
- 
+var circle_on_card = document.querySelector('.circle_on_card')
+
 function add_pig() {
     pigs_number = pigs_number + 1
-    var circle_on_card = document.querySelector('.circle_on_card')
     circle_on_card.innerHTML = pigs_number
 }
 function show_dialog() {
